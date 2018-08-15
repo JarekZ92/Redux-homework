6f61529004ddbd414ac595d3db7a55e138ffaba0
Jarek <z.jarek92@gmail.com>
2018-08-15 09:15:46 +0200

    added buttons into Calculator component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Counter from './views/Counter'
+import Calculator from './views/Calculator'
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Counter />
+        <Calculator />
       </div>
     )
   }
---
+++ src/views/Calculator.js
@@ -0,0 +1,39 @@
+// komponent z funkcji podłączony do connecta
+
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Calculator = (props) => (
+    <div>
+        <div>
+            <button>+</button>
+        </div>
+        <div>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+        </div>
+        <div>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+        </div>
+        <div>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+        </div>
+        <div>
+            <button>0</button>
+        </div>
+    </div>
+)
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => ({})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Calculator)
